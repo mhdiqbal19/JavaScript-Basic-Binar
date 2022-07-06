bc457c5db9fd0a339c1d994c51fb42c80d59c1a3
MUHAMMAD IQBAL <43297852+mhdiqbal19@users.noreply.github.com>
2022-07-06 11:34:49 +0700

    add case2 dan case4

---
+++ case2.js
@@ -0,0 +1,12 @@
+const bulan = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+var start = new Date();
+
+var end = new Date(new Date().getTime()+(540*24*60*60*1000));
+//console.info(end)
+
+var loop = new Date(start);
+while(loop <= end){        
+   var newDate = loop.setDate(loop.getDate() + 1);
+   loop = new Date(newDate);
+}
+document.writeln(`<h3>Hari ke 540 adalah Tanggal : ${loop.getDate()} ${bulan[loop.getMonth()]} ${loop.getFullYear()}</h3>`);  
---
+++ case4.js
@@ -0,0 +1,4 @@
+    var umurAyah = prompt('Masukan umur Ayah  :');
+
+    var umurAnak = umurAyah / 4;
+    console.log(`Umur Anak Adalah :  ${umurAnak} tahun`);
